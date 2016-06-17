3277c5a37e94c366676e827bacd41bb8c9bcf918
Koji Murata <koji.murata@dena.com>
2016-06-17 19:03:19 +0900

    example app

--- Example/SwiftyPluralizer/ViewController.swift
+++ Example/SwiftyPluralizer/ViewController.swift
@@ -7,18 +7,132 @@
 //
 
 import UIKit
+import SwiftyPluralizer
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  private let singulars = [
+    "search",
+    "switch",
+    "fix",
+    "box",
+    "process",
+    "address",
+    "case",
+    "stack",
+    "wish",
+    "fish",
+    "jeans",
+    "funky jeans",
+    "my money",
+    
+    "category",
+    "query",
+    "ability",
+    "agency",
+    "movie",
+    
+    "archive",
+    
+    "index",
+    
+    "wife",
+    "safe",
+    "half",
+    
+    "move",
+    
+    "salesperson",
+    "person",
+    
+    "spokesman",
+    "man",
+    "woman",
+    
+    "basis",
+    "diagnosis",
+    "diagnosis_a",
+    
+    "datum",
+    "medium",
+    "stadium",
+    "analysis",
+    "my_analysis",
+    
+    "node_child",
+    "child",
+    
+    "experience",
+    "day",
+    
+    "comment",
+    "foobar",
+    "newsletter",
+    
+    "old_news",
+    "news",
+    
+    "series",
+    "miniseries",
+    "species",
+    
+    "quiz",
+    
+    "perspective",
+    
+    "ox",
+    "photo",
+    "buffalo",
+    "tomato",
+    "dwarf",
+    "elf",
+    "information",
+    "equipment",
+    "bus",
+    "status",
+    "status_code",
+    "mouse",
+    
+    "louse",
+    "house",
+    "octopus",
+    "virus",
+    "alias",
+    "portfolio",
+    
+    "vertex",
+    "matrix",
+    "matrix_fu",
+    
+    "axis",
+    "taxi",
+    "testis",
+    "crisis",
+    
+    "rice",
+    "shoe",
+    
+    "horse",
+    "prize",
+    "edge",
+    
+    "database",
+    
+    "|ice",
+    "|ouse",
+    "slice",
+    "police",
+  ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return singulars.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("default", forIndexPath: indexPath)
+    let singular = singulars[indexPath.row]
+    cell.textLabel?.text = singular
+    cell.detailTextLabel?.text = "Plural: " + singular.plural
+    return cell
+  }
 }
 
