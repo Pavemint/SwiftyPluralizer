460e727a51282497dddc3ac8221589578d9f669e
Koji Murata <koji.murata@dena.com>
2016-06-17 18:48:04 +0900

    add test cases

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -7,11 +7,42 @@ import SwiftyPluralizer
 class TableOfContentsSpec: QuickSpec {
   
   override func spec() {
-    describe("these will fail") {
-      
-      it("can do maths") {
+    describe("to plural") {
+      it("empty plural") {
+        expect("".plural) == ""
+      }
+
+      it("singular to plural") {
         for (singular, plural) in TestCases.singularToPlural {
           expect(singular.plural) == plural
+        }
+      }
+      
+      it("pluralizing singulars") {
+        for (singular, plural) in TestCases.singularToPlural {
+          var s = singular
+          expect(s.pluralize()) == plural
+          expect(s) == plural
+        }
+      }
+    }
+    
+    describe("to singular") {
+      it("empty singular") {
+        expect("".singular) == ""
+      }
+
+      it("plural to singular") {
+        for (singular, plural) in TestCases.singularToPlural {
+          expect(plural.singular) == singular
+        }
+      }
+    
+      it("singularize plurals") {
+        for (singular, plural) in TestCases.singularToPlural {
+          var p = plural
+          expect(p.singularize()) == singular
+          expect(p) == singular
         }
       }
     }
